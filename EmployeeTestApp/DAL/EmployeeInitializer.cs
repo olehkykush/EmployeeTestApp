78c06079a3637f4e67be334791e4cbe3700e8fbc
Kykush Oleh <kykusholeg@gmail.com>
2015-10-02 13:35:02 +0300

    improvement to edit/delete, project cleanup

--- EmployeeTestApp/DAL/EmployeeInitializer.cs
+++ EmployeeTestApp/DAL/EmployeeInitializer.cs
@@ -15,17 +15,24 @@ namespace EmployeeTestApp.DAL
             {
             new Company{CompanyName="SymphonySolutions"},
             new Company{CompanyName="EPAM"},
-            new Company{CompanyName="SoftServe"}
+            new Company{CompanyName="SoftServe"},
+            new Company{CompanyName="Eleks"},
+            new Company{CompanyName="GlobalLogic"}
             };
             companies.ForEach(c => context.Companies.Add(c));
             context.SaveChanges();
             var employees = new List<Employee>
             {
-            new Employee{FirstMidName="Carson",LastName="Alexander", CompanyID=1},
-            new Employee{FirstMidName="Meredith",LastName="Alonso", CompanyID=1},
-            new Employee{FirstMidName="Arturo",LastName="Anand", CompanyID=1},
-            new Employee{FirstMidName="Gytis",LastName="Barzdukas", CompanyID=2},
-            new Employee{FirstMidName="Wo",LastName="Li", CompanyID=3}
+            new Employee{FirstMidName="Maxim",LastName="Wasylyshyn", CompanyID=1},
+            new Employee{FirstMidName="Oleksander",LastName="Zelenko", CompanyID=1},
+            new Employee{FirstMidName="Taras",LastName="Wasylyk", CompanyID=2},
+            new Employee{FirstMidName="Vasyl",LastName="Pasternak", CompanyID=2},
+            new Employee{FirstMidName="Maksim",LastName="Gavrilyuk", CompanyID=5},
+            new Employee{FirstMidName="Nazar",LastName="Chownyk", CompanyID=2},
+            new Employee{FirstMidName="Marko",LastName="Wolanski", CompanyID=4},
+            new Employee{FirstMidName="Oleksandr",LastName="Budny", CompanyID=1},
+            new Employee{FirstMidName="Marko",LastName="Stasiuk", CompanyID=4},
+            new Employee{FirstMidName="Taras",LastName="Kohut", CompanyID=3}
             };
 
             employees.ForEach(e => context.Employees.Add(e));
